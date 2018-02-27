bab9f7f0f6e4f630e7d32d312da7f7e88bf0ef00
seonguk-mac <su.seo@bakecompany.net>
2018-02-27 15:41:27 +0900

    매출 합계 기능 추가.

--- store/urls.py
+++ store/urls.py
@@ -1,13 +1,18 @@
 from django.urls import path
-from .views import TimeSalesListView, update_time_sales, DailySalesListView, update_daily_sales, DailySalesAnalysisView, SCDailySalesListView, BPDailySalesListView
+from .views import TimeSalesListView, update_time_sales, DailySalesListView, update_daily_sales, DailySalesAnalysisView, SCDailySalesListView, BPDailySalesListView,DailySalesForAllStoreListView
 
 app_name = 'store'
 urlpatterns =[
     path('sales/time/update', update_time_sales),
     path('sales/day', DailySalesListView.as_view(), name='daily_sales_list'),
+    path('sales/day/all',DailySalesForAllStoreListView.as_view(), name='daily_sales_all_list'),
     path('sales/day/update', update_daily_sales),
     path('sales/time', TimeSalesListView.as_view(), name='time_sales_list'),
     path('sales/analysis', DailySalesAnalysisView.as_view(), name='sales_analysis'),
+
+
+
+
     path('sc/sales/day', SCDailySalesListView.as_view(), name='sc_daily_sales'),
     path('bp/sales/day', BPDailySalesListView.as_view(), name = 'bp_daily_sales'),
 
--- store/views.py
+++ store/views.py
@@ -9,12 +9,30 @@ import os
 from django.conf import settings
 from .time_sales_crawler import get_daily_time_sales_for_all_store, get_token_kiosk
 from .daily_sales_crawler import update,update_everything
+from django.db.models import Sum
+
+
+class DailySalesForAllStoreListView(ListView):
+    model = DailySales
+    template_name = "store/dailysales_all.html"
+
+    def get_queryset(self):
+        date = self.request.GET.get('datepickerforsales')
+        try:
+            dt = datetime.datetime.strptime(date,'%Y-%m')
+        except:
+            dt = datetime.datetime.now()
+        daily_sales_q = DailySales.objects.filter(date__month=dt.month, date__year=dt.year).order_by('date').first()
+        print(daily_sales_q)
+        return DailySales.objects.filter(date__month=dt.month, date__year=dt.year).order_by('date')
 
 
 class DailySalesListView(ListView):
     model = DailySales
+    template_name = "store/dailysales_list.html"
 
-    def get_queryset(self):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
         if self.request.GET.get('radGroupBtn1_1'):
             q = self.request.GET.get('radGroupBtn1_1')
         else:
@@ -25,43 +43,46 @@ class DailySalesListView(ListView):
         except:
             dt = datetime.datetime.now()
         self.store = get_object_or_404(Store, slug=q)
-        print(date, dt)
-        return DailySales.objects.filter(store=self.store, date__month=dt.month, date__year=dt.year)
+        context['dailysales_list'] = DailySales.objects.filter(store=self.store, date__month=dt.month, date__year=dt.year)
+        context['aggregated_data'] = DailySales.objects.filter(store=self.store, date__month=dt.month, date__year=dt.year).aggregate(Sum('pos_cash_sales'),Sum('pos_card_sales'),Sum('kiosk_cash_sales'),Sum('kiosk_card_sales'),Sum('total_sales'))
+        return context
 
 
 class SCDailySalesListView(ListView):
     model = DailySales
     template_name = "store/dailysales_list_single_company.html"
 
-    def get_queryset(self):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
         date = self.request.GET.get('datepickerforsales')
         try:
             dt = datetime.datetime.strptime(date,'%Y-%m')
         except:
             dt = datetime.datetime.now()
         self.store = get_object_or_404(Store, slug='sc')
-        return DailySales.objects.filter(store=self.store, date__month=dt.month, date__year=dt.year)
-
+        context['dailysales_list'] = DailySales.objects.filter(store=self.store, date__month=dt.month, date__year=dt.year)
+        context['aggregated_data'] = DailySales.objects.filter(store=self.store, date__month=dt.month, date__year=dt.year).aggregate(Sum('pos_cash_sales'),Sum('pos_card_sales'),Sum('kiosk_cash_sales'),Sum('kiosk_card_sales'),Sum('total_sales'))
+        return context
 
 class BPDailySalesListView(ListView):
     model = DailySales
     template_name = "store/dailysales_list_single_company.html"
 
-    def get_queryset(self):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
         date = self.request.GET.get('datepickerforsales')
         try:
             dt = datetime.datetime.strptime(date,'%Y-%m')
         except:
             dt = datetime.datetime.now()
         self.store = get_object_or_404(Store, slug='bp')
-        print(self.store)
-        print(dt)
-        print(DailySales.objects.filter(store=self.store, date__month=dt.month, date__year=dt.year))
-        return DailySales.objects.filter(store=self.store, date__month=dt.month, date__year=dt.year)
+        context['dailysales_list'] = DailySales.objects.filter(store=self.store, date__month=dt.month, date__year=dt.year)
+        context['aggregated_data'] = DailySales.objects.filter(store=self.store, date__month=dt.month, date__year=dt.year).aggregate(Sum('pos_cash_sales'),Sum('pos_card_sales'),Sum('kiosk_cash_sales'),Sum('kiosk_card_sales'),Sum('total_sales'))
+        return context
 
 class DailySalesAnalysisView(ListView):
     model = DailySales
-
+    template_name = "store/dailysales_analysis.html"
     def get_queryset(self):
         day_string = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"]
         if self.request.GET.get('radGroupBtn1_1'):
@@ -76,13 +97,11 @@ class DailySalesAnalysisView(ListView):
             dt = datetime.datetime.now()
         print(dt)
         self.store = get_object_or_404(Store, slug=q)
-        print(DailySales.objects.filter(store=self.store, weekday=day_string[dt.weekday()]).order_by("date"))
-        return DailySales.objects.filter(store=self.store, weekday=day_string[dt.weekday()]).order_by("date")
+        return DailySales.objects.filter(store=self.store, weekday=day_string[dt.weekday()]).filter(date__lte=dt+datetime.timedelta(days=28), date__gte=dt+datetime.timedelta(days=-28)).order_by("date")
 
 
 class TimeSalesListView(ListView):
     model = TimeSales
-
     def get_queryset(self):
         if self.request.GET.get('radGroupBtn1_1'):
             q = self.request.GET.get('radGroupBtn1_1')
